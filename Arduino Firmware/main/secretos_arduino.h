9602ce881ee398f640a8b5fc6eae4fdb471b5c2f
mejillones <45486045+Edusantosi@users.noreply.github.com>
2023-11-15 10:33:22 +0100

    Update secretos_arduino.h

--- Arduino Firmware/main/secretos_arduino.h
+++ Arduino Firmware/main/secretos_arduino.h
@@ -1,16 +1,6 @@
-#define SECRET_SSID "devolo-fb5"
-#define SECRET_PASS "TNRNVRHAVVBXHIPM"
+#define SECRET_SSID "wifi_name"
+#define SECRET_PASS "wifi_psswd"
 #define SECRET_BROKER "mqtt.thethings.io"
-#define SECRET_TOPIC "v2/things/BJdwa3etlM0MCJCM-pHlA1zVZ-pdP8bPz4TO9fqQilM"
+#define SECRET_TOPIC "v2/things/token"
 
-//MOVISTAR_E8D8
-//U9x7U4wv9JArCEUFy7Cy
 
-//devolo-fb5
-//TNRNVRHAVVBXHIPM
-
-//MIWIFI_ii9S
-//tfwe9zSg
-
-//Token
-//BJdwa3etlM0MCJCM-pHlA1zVZ-pdP8bPz4TO9fqQilM
